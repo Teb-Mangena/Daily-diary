b45ca10e6ab62f9d8d07da18e56022b832fc23e6
Tebatso <tebatsomangena8@gmail.com>
2025-03-31 06:22:44 +0200

    placed name and surname of the user on to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const {logout} = useLogout();
+  const {user} = useAuthContext();
 
   const handleLogOut = () => {
     logout();
@@ -19,6 +21,12 @@ const Navbar = () => {
       </div>
 
       <div className="right-section">
+        {user && (
+          <div className="userDetails">
+            <span className="username">{user.name} </span>  
+            <span className="lastName">{user.lastName}</span>
+          </div>
+        )}
         <ul className="nav-links">
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/admin-dashboard'>admin dashboard</Link></li>
@@ -26,11 +34,12 @@ const Navbar = () => {
           <li><Link to='/login'>login</Link></li>
           <li><Link to='/signup'>signup</Link></li>
         </ul>
-      </div>
 
-      <button className="btn-logout" onClick={handleLogOut}>
+        <button className="btn-logout" onClick={handleLogOut}>
         Logout
       </button>
+
+      </div>
     </nav>
    );
 }
