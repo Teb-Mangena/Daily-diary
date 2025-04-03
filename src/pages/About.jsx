e720aece3e51919c478562dc09c357b0c0cc4b4c
Tebatso <tebatsomangena8@gmail.com>
2025-04-03 09:49:31 +0200

    about us changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,9 +24,9 @@ const About = () => {
           </p>
           <p>
             All user data is highly protected using end-to-end encryption,
-            ensuring that your entries remain confidential and secure. Daily Diary's top
-            priority is safeguarding your privacy, so you can focus on writing
-            without worrying about your secrets being exposed.
+            ensuring that your entries remain confidential and secure. Daily
+            Diary's top priority is safeguarding your privacy, so you can focus
+            on writing without worrying about your secrets being exposed.
           </p>
           <p>With Daily Diary, you can:</p>
           <ul>
@@ -40,6 +40,23 @@ const About = () => {
           <p>
             Join the Daily Diary community today and start writing your story!
           </p>
+
+          <h2>Contact Me</h2>
+          <p>
+            If you have any questions, suggestions, or just want to say hello,
+            feel free to reach out to me:
+          </p>
+          <ul>
+            <li>
+              Email:{" "}
+              <a href="mailto:tebatsomangena8@gmail.com">
+                tebatsomangena8@gmail.com
+              </a>
+            </li>
+            <li>
+              WhatsApp: <a href="tel:+27649494191">064 949 4191</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
